c6a1af80d7f0e67adf156f3c41293bb87dc1a968
Josh 'Panda' Edwards <josh.dean.edwards@gmail.com>
2023-10-14 18:11:42 -0500

    Added authentication and authorization with JWT on API

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -1,6 +1,7 @@
 using API.Errors;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
@@ -32,6 +33,9 @@ public static class ApplicationServicesExtensions
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IBasketRepository, BasketRepository>();
         services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        
+        // Services 
+        services.AddScoped<ITokenService, TokenService>();
         
         // Datamapper
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
